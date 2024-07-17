9159320e15ce770b27e50ab1b1008fb54fdc02f3
Martoto <dansal@alunos.utfpr.edu.br>
2024-07-17 19:15:19 -0300

    😜

--- icia/settings/dev.py
+++ icia/settings/dev.py
@@ -30,10 +30,10 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'mypassword',
-        'HOST': 'localhost',
-        'PORT': '8080',
+        'NAME': '*******',
+        'USER': '*******',
+        'PASSWORD': '*******',
+        'HOST': '*******',
+        'PORT': '*******',
     }
 }
