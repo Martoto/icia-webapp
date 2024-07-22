266b5580aaadd394c3adbf2a60e5a5212c889da0
Martoto <dansal@alunos.utfpr.edu.br>
2024-07-21 22:29:22 -0300

    reorg settings

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icia.settings.dev')
+    environment = os.getenv('DJANGO_ENV', 'dev')  # Default to 'dev' if not set
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icia.settings.'+environment)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
