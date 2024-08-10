e87dec5c29e079ab9be9831e6510c05b2d70fda9
Martoto <dansal@alunos.utfpr.edu.br>
2024-08-10 16:51:42 -0300

    signup

--- icia/urls.py
+++ icia/urls.py
@@ -21,10 +21,11 @@ from django.conf.urls.static import static
 from django.views.generic.base import TemplateView 
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
-    path('polls/', include('polls.urls')),
-    path("accounts/", include("django.contrib.auth.urls")),  
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('polls/', include('polls.urls')),
+    path("accounts/", include("accounts.urls")),  
+    path("accounts/", include("django.contrib.auth.urls")),  
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+] 
 
 
