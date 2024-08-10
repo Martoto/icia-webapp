d00ef0b8179fe66890981acb01477bcac821e84c
Martoto <dansal@alunos.utfpr.edu.br>
2024-08-10 20:29:37 -0300

    about page

--- icia/urls.py
+++ icia/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),  
     path("accounts/", include("django.contrib.auth.urls")),  
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("about", TemplateView.as_view(template_name="about.html"), name="about"),
 ] 
 
 
