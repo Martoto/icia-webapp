4a2677ab2ef843bb15d6b42c985396ed1728c6fd
Martoto <dansal@alunos.utfpr.edu.br>
2024-08-20 00:34:45 -0300

    Models for estimate type questions

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.TextField(max_length=5000)
     pub_date = models.DateTimeField("date published")
     available = models.BooleanField(default=True)
     def was_published_recently(self):
@@ -25,6 +25,21 @@ class Choice(models.Model):
 class Vote(models.Model):
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE, db_index=True)
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    description = models.TextField(null=True, max_length=200)
+
+class Classification(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    range = models.DecimalField(max_digits=5,decimal_places=2, default=100.00)
+    score = models.BigIntegerField(default=1)
+    def get_estimate(self):
+        estimates = self.estimate_set.all()
+        return sum(estimate.value for estimate in estimates)/(estimates.len)
+
+class Estimate(models.Model):
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE, db_index=True)
+    classification = models.ForeignKey(Classification, on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=5,decimal_places=2,default=50.00)
+    description = models.TextField(null=True, max_length=200)
 
 class AgentPost(models.Model):
     owner = models.ForeignKey(Agent, on_delete=models.CASCADE, db_index=True)
