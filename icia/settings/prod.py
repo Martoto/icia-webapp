b576696f6eeff0ee4bf4ddb883b16e0760eca9a1
Martoto <dansal@alunos.utfpr.edu.br>
2024-08-25 00:02:56 -0300

    removed file compression

--- icia/settings/prod.py
+++ icia/settings/prod.py
@@ -51,7 +51,6 @@ DATABASES = {
     }
 }
 
-STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage') 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
