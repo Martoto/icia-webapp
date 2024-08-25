9343726fbc4968676b8b5538e4b5e654bed3545a
Martoto <dansal@alunos.utfpr.edu.br>
2024-08-24 21:44:14 -0300

    Revert "removed whitenoise"
    This reverts commit 117cadcd40a74c9c6c4294127f76ab781c596b3f.

--- icia/settings/prod.py
+++ icia/settings/prod.py
@@ -51,6 +51,8 @@ DATABASES = {
     }
 }
 
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage') 
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
