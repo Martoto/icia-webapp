768da2bb8f66a4341d36779d5d518a0d89b0f003
Martoto <dansal@alunos.utfpr.edu.br>
2024-08-31 19:41:44 -0300

    dockerization

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.conf import settings
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['registration_open'] = True if settings.REGISTRATION_OPEN == True else False
+
+        return context 
