54e653deb35a6a3eccf734ba2233aa89b5910948
Martoto <dansal@alunos.utfpr.edu.br>
2024-09-18 22:47:57 -0300

    random change to trigger actions

--- icia/settings/settings.py
+++ icia/settings/settings.py
@@ -113,9 +113,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'icia.wsgi.application'
 
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
